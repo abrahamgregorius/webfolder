ce718c6b608139ab37d270f1ef3bf0ee54970551
abrahamgregorius <athioii6@gmail.com>
2025-01-16 22:33:01 +0700

    feat: new color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,7 @@ import githubimg from '../assets/github.png'
 function Card({title, description, src, link, tech}) {
     return(
         <>
-            <div className="card select-none shadow-xl hover:shadow-2xl transition duration-200 border-white border-2 min-[420px]:border rounded p-3">
+            <div className="card select-none shadow-zinc-200 border-white border-2 min-[420px]:border rounded p-3">
                 <a target={link ? "_blank" : ""} href={link ? link : "#"}>
                     <div className="project-head">
                         <img className="h-[0px] w-full rounded object-top object-cover" src={src} alt="" />
