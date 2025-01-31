2167da96ecab034aea502774a3a6bd7d9bca0058
abrahamgregorius <athioii6@gmail.com>
2025-01-31 14:55:57 +0700

    feat: user-select

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,16 +13,16 @@ function Navbar() {
 
     return(
         <>
-        <div className="navbar flex justify-between items-center relative font-semibold">
-                <div className="nav-left">
+        <div className="navbar select-none flex justify-between items-center relative font-semibold">
+                <div className="nav-left cursor-pointer">
                     <Link to="/" className="font-black font-mono">abrahamgregorius</Link>
                 </div>
                 <div className="nav-toggle min-[420px]:hidden flex gap-3">
-                    <span className="text-2xl" onClick={() => setIsNavbarClicked(!isNavbarClicked)}>&equiv;</span>
+                    <span className="text-2xl cursor-pointer" onClick={() => setIsNavbarClicked(!isNavbarClicked)}>&equiv;</span>
                 </div>
                 <div className="nav-right max-[420px]:hidden flex gap-3">
                     <NavLink className={"text-sm hover:underline transition"} to="/projects">projects</NavLink>
-                    <NavLink className={"text-sm hover:underline transition"} to="/achievements">achievements</NavLink>
+                    <NavLink className={"text-sm cursor-pointer hover:underline transition"} to="/achievements">achievements</NavLink>
                 </div>
             </div>
             <div className={`nav-toggle-menu text-lg h-[17vh] text-right font-normal border-b gap-2 mb-2 mt-2 w-full flex  px-4 justify-center flex-col min-[420px]:hidden ${isNavbarClicked ? "flex" : "hidden"}`}>
